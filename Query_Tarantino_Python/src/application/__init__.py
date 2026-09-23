6689938f091d3d6ef95a68a92975c9bfe39a1730
Hugo <r6ysfthqbm@privaterelay.appleid.com>
2026-09-23 13:43:24 +0100

    init application module fix

--- Query_Tarantino_Python/src/application/__init__.py
+++ Query_Tarantino_Python/src/application/__init__.py
@@ -1,11 +1,11 @@
-# src/application/__init__.py
+
 
 from .ingest_book_use_case import IngestBookUseCase
-from .control_pipeline import ControlPipeline, IndexUseCase, BookIdProvider
+from .control_pipeline import ControlPipeline
+from .index_book_use_case import IndexBookUseCase
 
 __all__ = [
     "IngestBookUseCase",
     "ControlPipeline",
-    "IndexUseCase",
-    "BookIdProvider"
+    "IndexBookUseCase"
 ]
